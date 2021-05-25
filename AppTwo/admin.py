56536c774070dba36f8065cf47507b2fb97f617d
Anna Eklund <vitastjern@gmail.com>
2021-05-25 16:20:14 +0200

    Added a bit more informatic listings to admin page

--- AppTwo/admin.py
+++ AppTwo/admin.py
@@ -1,11 +1,24 @@
 from django.contrib import admin
-from AppTwo.models import AccessRecord,Topic,Webpage
+from .models import AccessRecord,Topic,Webpage
 
 # Register your models here.
 
-admin.site.register(AccessRecord)
-admin.site.register(Topic)
-admin.site.register(Webpage)
+@admin.register(AccessRecord)
+class AccessRecordAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'date')
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ('id', 'top_name')
+
+@admin.register(Webpage)
+class WebpageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'topic', 'name', 'url')
+
+# This version uses the __str__ function in models instead 
+# admin.site.register(AccessRecord)
+# admin.site.register(Topic)
+# admin.site.register(Webpage)
 
 
 
